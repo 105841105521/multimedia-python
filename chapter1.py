9a899034693bdfd568fdcb527859e93154d2c3fe
105841105521 <138638317+105841105521@users.noreply.github.com>
2024-08-07 16:22:20 +0000

    chapter1 & chapter2

---
+++ chapter1.py
@@ -0,0 +1,10 @@
+import pygame
+from PIL import Image
+import cv2
+import pydub
+import tkinter as tk
+
+import moviepy.editor as mp
+
+# Konfirmasi instalasi
+print("✅ Semua pustaka telah terinstal dengan benar!")
